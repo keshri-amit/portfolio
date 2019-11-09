0aca504ee7ca57e7079c55250a483401770e75f8
amit-keshri <sonunikki8819@gmail.com>
2019-11-09 13:36:52 +0530

    add project

--- precache-manifest.f6ccb1f4dc629d5469b9296a3a7663b3.js
+++ precache-manifest.f6ccb1f4dc629d5469b9296a3a7663b3.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
     "revision": "5f748d24129568e2a47cc748f42836f4",
-    "url": "static/media/web2.5f748d24.jpg"
+    "url": "/static/media/web2.5f748d24.jpg"
   },
   {
     "revision": "469bc887d33b0796259f",
@@ -9,7 +9,7 @@ self.__precacheManifest = [
   },
   {
     "revision": "6bd4f21594238db8010ad9dac0c2ff67",
-    "url": "static/media/banner.6bd4f215.jpg"
+    "url": "/static/media/banner.6bd4f215.jpg"
   },
   {
     "revision": "a2af1d7313ac686c3f4f",
@@ -21,11 +21,11 @@ self.__precacheManifest = [
   },
   {
     "revision": "cfea4090796865c0eede8e60acfae80e",
-    "url": "static/media/sonu.cfea4090.jpg"
+    "url": "/static/media/sonu.cfea4090.jpg"
   },
   {
     "revision": "479f09d61b9c4d363b91f6fc5719fe9b",
-    "url": "static/media/web1.479f09d6.jpg"
+    "url": "/static/media/web1.479f09d6.jpg"
   },
   {
     "revision": "469bc887d33b0796259f",
@@ -33,19 +33,19 @@ self.__precacheManifest = [
   },
   {
     "revision": "1fe6c5f615eace8fdf78b71499750414",
-    "url": "static/media/web3.1fe6c5f6.jpg"
+    "url": "/static/media/web3.1fe6c5f6.jpg"
   },
   {
     "revision": "c05fae1eb4912373c48d5bc89b33564b",
-    "url": "static/media/web4.c05fae1e.jpg"
+    "url": "/static/media/web4.c05fae1e.jpg"
   },
   {
     "revision": "c18d841bacdd1e782ff7491e3b80097c",
-    "url": "static/media/web5.c18d841b.jpg"
+    "url": "/static/media/web5.c18d841b.jpg"
   },
   {
     "revision": "3d93b7b16e6c82615eb6c864688144cf",
-    "url": "static/media/web6.3d93b7b1.jpg"
+    "url": "/static/media/web6.3d93b7b1.jpg"
   },
   {
     "revision": "a2af1d7313ac686c3f4f",
